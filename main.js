47497f321e3a30cfe19e69b73b1d94a2bcf39f09
Julian Lepinski <lepinski@gmail.com>
2015-07-21 12:09:29 -0700

    code cleanup

--- main.js
+++ main.js
@@ -10,44 +10,22 @@ if (!process.env.PORT){
 console.log("");
 console.log("cast-aws-proxy starting up...");
 
-var http			= require("http");
-var httpProxy		= require("http-proxy");
+var http          = require("http");
+var httpProxy		  = require("http-proxy");
 
-var instanceName	= "heroku-cast-aws-proxy-" + uniqueToken(3);
-var targetURI		= process.env.ORIGIN_URI;
-
+var targetURI		  = process.env.ORIGIN_URI;
 var forcedHeaders	= require("./forced-headers.json");
 
 var proxy = httpProxy.createProxyServer({});
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  for (var i in forcedHeaders) res.setHeader(i, forcedHeaders[i]);
+});
+
+console.log("forced headers follow:");
+console.dir(forcedHeaders);
+
+console.log("proxying requests at port " + process.env.PORT + " to " + targetURI);
 var server = http.createServer(function(req, res) {
   proxy.web(req, res, { target: targetURI, secure: false });
 });
-
 server.listen(process.env.PORT);
-
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  for (var i in forcedHeaders){
-  	res.setHeader(i, forcedHeaders[i]);
-  }
-});
-
-console.log("");
-console.log("forced headers follow:");
-console.dir(forcedHeaders);
-console.log("");
-
-console.log("proxying requests at port " + process.env.PORT + " to " + targetURI);
-
-/////////////////////
-// helpers/prototypes
-/////////////////////
-
-function uniqueToken(length) {
-    var fullToken = (Math.random() * new Date().getTime()).toString(36).toUpperCase().replace(/\./g, '-');
-    if (length === null){
-        return fullToken;
-    } else {
-        var shortenedToken = fullToken.substring(0, length);
-        return shortenedToken;
-    }
-}
