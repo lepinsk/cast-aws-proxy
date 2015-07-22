a73415c9443b9e3169d98295fe3e3ae12a5a03f6
Julian Lepinski <lepinski@gmail.com>
2015-07-22 13:43:30 -0700

    fuck it, we'll roll our own

--- main.js
+++ main.js
@@ -11,22 +11,18 @@ console.log("");
 console.log("cast-aws-proxy starting up...");
 
 var http			= require("http");
-var httpProxy		= require("http-proxy");
+var express			= require("express");
+var request			= require("request");
 
+var app				= express();
 var targetURI		= process.env.ORIGIN_URI;
 var forcedHeaders	= require("./forced-headers.json");
 
-var proxy = httpProxy.createProxyServer({});
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  console.dir(proxyReq);
-  for (var i in forcedHeaders) res.setHeader(i, forcedHeaders[i]);
-});
+console.log("forced headers: " + JSON.stringify(forcedHeaders));
 
-console.log("forced headers follow:");
-console.dir(forcedHeaders);
+app.get("*", function (req, res) {
+	request(targetURI + req.url).pipe(res);
+});
 
 console.log("proxying requests at port " + process.env.PORT + " to " + targetURI);
-var server = http.createServer(function(req, res) {
-  proxy.web(req, res, { target: targetURI, secure: false });
-});
-server.listen(process.env.PORT);
+var server = app.listen(process.env.PORT);
