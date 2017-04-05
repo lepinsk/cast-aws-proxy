2f17ae575aa8fd6083eacd4311cdb6796c9dbb91
Julian Lepinski <lepinski@gmail.com>
2017-04-05 15:53:51 -0400

    server: more cleanup; adding remote headers & app.json

--- main.js
+++ main.js
@@ -19,22 +19,44 @@ var targetURI		= process.env.ORIGIN_URI;
 var forceOverwite	= process.env.OVERWRITE_EXISTING ? true : false;
 var forcedHeaders	= require("./forced-headers.json");
 var verboseLogging	= process.env.VERBOSE_LOGS ? true : false;
+var remoteHeaders	= process.env.REMOTE_FORCED_HEADERS ? true : false;
 
-if (!targetURI){
+if (!targetURI) {
 	console.log("[error] no ORIGIN_URI present");
 	process.exit(1);
 }
 
-console.log("[config] forced headers: " + JSON.stringify(forcedHeaders));
-console.log("[config] overwrite existing headers: " + forceOverwite);
-console.log("[config] verbose logging: " + verboseLogging);
+if (remoteHeaders) {
+	var remoteHeaderURL = process.env.REMOTE_FORCED_HEADERS;
+	console.log("[config] loading remote headers from " + remoteHeaderURL);
+	request(remoteHeaderURL, function (error, response, body) {
+		if (error) {
+			console.log("[error] there was a problem getting remote headers: " + error);
+			process.exit(1);
+		} else if (response && response.statusCode === 200) {
+			try {
+				forcedHeaders = JSON.parse(body);
+				console.log("[config] remote headers loaded");
+				completeAsyncStartup();
+			} catch (e) {
+				console.log("[error] there was a problem parsing the remote headers: " + e);
+				process.exit(1);
+			}
+		} else {
+			console.log("[error] there was an unspecified problem getting the remote headers");
+			process.exit(1);
+		}
+	});
+} else {
+	completeAsyncStartup();
+}
 
 app.get("*", function (req, res) {
 	verboseLog("[" + req.url + "] proxying request to " + targetURI + req.url);
 	request(targetURI + req.url).pipe(res);
 
 	res.oldWriteHead = res.writeHead;
-	res.writeHead = function(statusCode, reasonPhrase, headers){
+	res.writeHead = function(statusCode, reasonPhrase, headers) {
 		for (headerKey in forcedHeaders) {
 			if (!res.get(headerKey) || forceOverwite) { 
 				res.header(headerKey, forcedHeaders[headerKey]);
@@ -47,10 +69,15 @@ app.get("*", function (req, res) {
 	}
 });
 
-function verboseLog(log){
+function verboseLog (log) {
 	if (!verboseLogging) return;
 	console.log(log);
 }
 
-console.log("[init] proxying requests at port " + process.env.PORT + " to " + targetURI);
-var server = app.listen(process.env.PORT);
+function completeAsyncStartup() {
+	console.log("[config] forced headers: " + JSON.stringify(forcedHeaders));
+	console.log("[config] overwrite existing headers: " + forceOverwite);
+	console.log("[config] verbose logging: " + verboseLogging);
+	console.log("[init] proxying requests at port " + process.env.PORT + " to " + targetURI);
+	app.listen(process.env.PORT);
+}
