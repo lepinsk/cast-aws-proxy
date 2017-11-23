75cdef898f3ce976f921ee8e830a6a8d8e23d0af
Julian Lepinski <lepinski@gmail.com>
2017-11-23 12:20:43 -0800

    meta: updating code indentation
    improving github display 🙄

--- main.js
+++ main.js
@@ -1,88 +1,88 @@
 // cast-aws-proxy
 // main.js
-// julian lepinski 2015
+// julian lepinski 2015-2017
 
 if (!process.env.PORT){
-	try {
-		var localConfig = require("./.local-config.json");
-		for (var i in localConfig) process.env[i] = localConfig[i];
-	} catch (e) {
-		console.log("[error] no process.env.PORT or .local-config.json found; if you're running locally this app needs some config vars");
-		process.exit(1);
-	}
+   try {
+      var localConfig = require("./.local-config.json");
+      for (var i in localConfig) process.env[i] = localConfig[i];
+   } catch (e) {
+      console.log("[error] no process.env.PORT or .local-config.json found; if you're running locally this app needs some config vars");
+      process.exit(1);
+   }
 }
 
 console.log("");
 console.log("[init] cast-aws-proxy starting up...");
 
-var http			= require("http");
-var express			= require("express");
-var request			= require("request");
+var http            = require("http");
+var express         = require("express");
+var request         = require("request");
 
-var app				= express();
-var targetURI		= process.env.ORIGIN_URI;
-var forceOverwite	= process.env.OVERWRITE_EXISTING ? true : false;
-var forcedHeaders	= require("./forced-headers.json");
-var verboseLogging	= process.env.VERBOSE_LOGS ? true : false;
-var remoteHeaders	= process.env.REMOTE_FORCED_HEADERS ? true : false;
+var app             = express();
+var targetURI       = process.env.ORIGIN_URI;
+var forceOverwite   = process.env.OVERWRITE_EXISTING ? true : false;
+var forcedHeaders   = require("./forced-headers.json");
+var verboseLogging  = process.env.VERBOSE_LOGS ? true : false;
+var remoteHeaders   = process.env.REMOTE_FORCED_HEADERS ? true : false;
 
 if (!targetURI) {
-	console.log("[error] no ORIGIN_URI present");
-	process.exit(1);
+   console.log("[error] no ORIGIN_URI present");
+   process.exit(1);
 }
 
 if (remoteHeaders && process.env.REMOTE_FORCED_HEADERS.trim() !== "") {
-	var remoteHeaderURL = process.env.REMOTE_FORCED_HEADERS;
-	console.log("[config] loading remote headers from " + remoteHeaderURL);
-	request(remoteHeaderURL, function (error, response, body) {
-		if (error) {
-			console.log("[error] there was a problem getting remote headers: " + error);
-			process.exit(1);
-		} else if (response && response.statusCode === 200) {
-			try {
-				forcedHeaders = JSON.parse(body);
-				console.log("[config] remote headers loaded");
-				completeAsyncStartup();
-			} catch (e) {
-				console.log("[error] there was a problem parsing the remote headers: " + e);
-				process.exit(1);
-			}
-		} else {
-			console.log("[error] there was an unspecified problem getting the remote headers");
-			process.exit(1);
-		}
-	});
+   var remoteHeaderURL = process.env.REMOTE_FORCED_HEADERS;
+   console.log("[config] loading remote headers from " + remoteHeaderURL);
+   request(remoteHeaderURL, function (error, response, body) {
+      if (error) {
+         console.log("[error] there was a problem getting remote headers: " + error);
+         process.exit(1);
+      } else if (response && response.statusCode === 200) {
+         try {
+            forcedHeaders = JSON.parse(body);
+            console.log("[config] remote headers loaded");
+            completeAsyncStartup();
+         } catch (e) {
+            console.log("[error] there was a problem parsing the remote headers: " + e);
+            process.exit(1);
+         }
+      } else {
+         console.log("[error] there was an unspecified problem getting the remote headers");
+         process.exit(1);
+      }
+   });
 } else {
-	completeAsyncStartup();
+   completeAsyncStartup();
 }
 
 app.get("*", function (req, res) {
-	verboseLog("[" + req.url + "] proxying request to " + targetURI + req.url);
-	request(targetURI + req.url).pipe(res);
+   verboseLog("[" + req.url + "] proxying request to " + targetURI + req.url);
+   request(targetURI + req.url).pipe(res);
 
-	res.oldWriteHead = res.writeHead;
-	res.writeHead = function(statusCode, reasonPhrase, headers) {
-		for (headerKey in forcedHeaders) {
-			if (!res.get(headerKey) || forceOverwite) { 
-				res.header(headerKey, forcedHeaders[headerKey]);
-				verboseLog("[" + req.url + "] setting '" + headerKey + "' to '" + forcedHeaders[headerKey] + "'");
-			} else {
-				verboseLog("[" + req.url + "] not setting '" + headerKey + "', it exists and forceOverwrite=false");
-			}
-		}
-		res.oldWriteHead(statusCode, reasonPhrase, headers);
-	}
+   res.oldWriteHead = res.writeHead;
+   res.writeHead = function(statusCode, reasonPhrase, headers) {
+      for (headerKey in forcedHeaders) {
+         if (!res.get(headerKey) || forceOverwite) { 
+            res.header(headerKey, forcedHeaders[headerKey]);
+            verboseLog("[" + req.url + "] setting '" + headerKey + "' to '" + forcedHeaders[headerKey] + "'");
+         } else {
+            verboseLog("[" + req.url + "] not setting '" + headerKey + "', it exists and forceOverwrite=false");
+         }
+      }
+      res.oldWriteHead(statusCode, reasonPhrase, headers);
+   }
 });
 
 function verboseLog (log) {
-	if (!verboseLogging) return;
-	console.log(log);
+   if (!verboseLogging) return;
+   console.log(log);
 }
 
 function completeAsyncStartup() {
-	console.log("[config] forced headers: " + JSON.stringify(forcedHeaders));
-	console.log("[config] overwrite existing headers: " + forceOverwite);
-	console.log("[config] verbose logging: " + verboseLogging);
-	console.log("[init] proxying requests at port " + process.env.PORT + " to " + targetURI);
-	app.listen(process.env.PORT);
+   console.log("[config] forced headers: " + JSON.stringify(forcedHeaders));
+   console.log("[config] overwrite existing headers: " + forceOverwite);
+   console.log("[config] verbose logging: " + verboseLogging);
+   console.log("[init] proxying requests at port " + process.env.PORT + " to " + targetURI);
+   app.listen(process.env.PORT);
 }
